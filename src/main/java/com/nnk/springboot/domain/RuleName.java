0cb2d8f0b2606a1b6d4a16a5e441a4f7ce10b444
Musapa <ajagodic1990@gmail.com>
2020-07-14 09:37:39 +0200

    Create controoler classes and templates for Trade and ruleName

--- src/main/java/com/nnk/springboot/domain/RuleName.java
+++ src/main/java/com/nnk/springboot/domain/RuleName.java
@@ -18,6 +18,9 @@ public class RuleName {
 	private String sqlStr;
 	private String sqlPart;
 
+	public RuleName() {
+	}
+	
 	public RuleName(String name, String description, String json, String template, String sqlStr, String sqlPart) {
 		this.name = name;
 		this.description = description;
