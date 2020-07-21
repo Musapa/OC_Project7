ffd59a4edf84a8e6f54dbe4634145c1a1ea957ca
Musapa <ajagodic1990@gmail.com>
2020-07-21 09:51:50 +0200

    update CurvePointTest

--- src/test/java/com/nnk/springboot/CurvePointTests.java
+++ src/test/java/com/nnk/springboot/CurvePointTests.java
@@ -27,12 +27,12 @@ public class CurvePointTests {
 		// Save
 		curvePoint = curvePointRepository.save(curvePoint);
 		Assert.assertNotNull(curvePoint.getId());
-		Assert.assertTrue(curvePoint.getCurveId() == 10);
+		Assert.assertTrue(curvePoint.getCurveId().equals(10));
 
 		// Update
 		curvePoint.setCurveId(20);
 		curvePoint = curvePointRepository.save(curvePoint);
-		Assert.assertTrue(curvePoint.getCurveId() == 20);
+		Assert.assertTrue(curvePoint.getCurveId().equals(20));
 
 		// Find
 		List<CurvePoint> listResult = curvePointRepository.findAll();
